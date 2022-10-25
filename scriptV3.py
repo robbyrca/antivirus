9a629bc4fe2e1a0aa6ca0a3cf6852b88901de911
Rubén Cuenca Altamirano <rcuenca.alt@gmail.com>
2022-10-25 18:46:46 +0200

    Update scriptV3.py
    añadir enviar archivo a api

--- scriptV3.py
+++ scriptV3.py
@@ -1,4 +1,4 @@
-import shutil,os
+import shutil,os,requests
 
 file_source = '/home/ubunserver/github/antivirus/virustotal/'
 file_destination1 = '/home/ubunserver/github/antivirus/virustotalrevisando/'
@@ -18,3 +18,9 @@ for root, dirs, files in os.walk(file_source):
 
 shutil.rmtree(file_source)
 os.mkdir(file_source)
+
+url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+params = {'apikey': '206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7'}
+files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
+response = requests.post(url, files=files, params=params)
+print(response.json())
