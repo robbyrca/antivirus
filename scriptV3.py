6e4e52a126008ebe5d27008a315049eafbfb187e
robbyrca <rcuenca.alt@gmail.com>
2022-10-25 16:06:57 +0200

    Create scriptV3.py
    sincronizacion version 3

---
+++ scriptV3.py
@@ -0,0 +1,21 @@
+import shutil,os
+
+file_source = '/home/ubunserver/antivirus/virustotal/'
+file_destination1 = '/home/ubunserver/antivirus/virustotalrevisando/'
+file_destination2 = '/home/ubunserver/antivirus/virustotalrevisado/'
+file_destination3 = '/home/ubunserver/antivirus/virustotalcuarentena/'
+
+get_file = os.listdir(file_source)
+
+contador=1
+
+for x in get_file:
+   contador=contador+1                                #MEJORAS V2.2 nº1 (limiracion de lectura de archivos a 10)
+   if contador <11 :                                  #MEJORAS V2.2 nº1
+    shutil.move(file_source + x, file_destination1)   
+    if get_file:
+        print (x + 'moved')
+    else:
+        f = open ('logs.txt', "w")
+        f.write('Error moving ' + x)
+        f.close()
