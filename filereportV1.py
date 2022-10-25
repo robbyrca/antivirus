413dbfe03a7954f641a5eb15641304aa37a31fba
robbyrca <rcuenca.alt@gmail.com>
2022-10-25 22:29:39 +0200

    archivos generados por mac

--- filereportV1.py
+++ filereportV1.py
@@ -28,8 +28,8 @@ def idsave(id,file):
 for root, dirs, files in os.walk(file_source):
     for filename in files:
         filepath = os.path.join(root, filename)
-        with open(filepath+filename, 'r') as f:
-            contenido = f.read()
+        with open(filepath+filename, 'r') as r:
+            contenido = r.read()
         id = contenido.split(":", 1)
         print(id)
 
