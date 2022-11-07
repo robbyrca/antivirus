802d1aaf5721103434c95b462a5107c385610879
robbyrca <rcuenca.alt@gmail.com>
2022-11-07 16:03:55 +0100

    Update filereportV1.py
    añadir una carpeta

--- filereportV1.py
+++ filereportV1.py
@@ -5,6 +5,7 @@ file_source = '/home/user/github/antivirus/virustotalid/'
 file_destination1 = '/home/user/github/antivirus/virustotalrevisando/'
 file_destination2 = '/home/user/github/antivirus/virustotalrevisado/'
 file_destination3 = '/home/user/github/antivirus/virustotalcuarentena/'
+file_destination4 = 'home/user/github/antivirus/logs/'
 
 def upload(id):
     url = "https://www.virustotal.com/api/v3/analyses/"+id
@@ -26,7 +27,7 @@ def upload(id):
 
 def responsesave(jsonresp, file):
     print(jsonresp)
-    with open(file_destination2+id, "w") as fp:
+    with open(file_destination4+id, "w") as fp:
         json.dump(jsonresp, fp, indent=2)
 
 for root, dirs, files in os.walk(file_source):
