36704e4839afa3da56b2ff0182235c33ced62654
robbyrca <rcuenca.alt@gmail.com>
2022-11-10 15:36:31 +0100

    Update filereportV1.py
    recoger malicious

--- filereportV1.py
+++ filereportV1.py
@@ -14,8 +14,10 @@ def upload(id):
         "x-apikey": "206706e5d63a9393a5786e3191ba9c471dcbb00305f4a32d49de38c45f20c4c7"
     }
     response = requests.get(url, headers=headers)
-    print(response.text)
+    #print(response.text)
     jsonresp = response.json()
+    malget = jsonresp.get("data").get("stats").get("malicious")
+    print (malget)
     responsesave(jsonresp,id)
     #print(jsonresp)
     #malcont = jsonresp.get("data").get("id")
