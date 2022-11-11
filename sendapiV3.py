8afe0bf325dfa32420b1aee0c04e37601564ac50
robbyrca <rcuenca.alt@gmail.com>
2022-11-11 17:45:25 +0100

    Update sendapiV3.py
    probar una cosita

--- sendapiV3.py
+++ sendapiV3.py
@@ -62,14 +62,15 @@ for root, dirs, files in os.walk(file_source):
     for filename in files:
         filepath = os.path.join(root, filename)
         #print(file_destination1+filename)
-        if (os.path.getsize(os.path.join(root, filename)) >> 20) > 32:
-            shutil.move(filepath, file_destination1)
-            print("\n"+file_destination1+filename + " moved")
-            uploadbig(file_destination1+filename)
-        else:
-            shutil.move(filepath, file_destination1)
-            print("\n"+file_destination1+filename + " moved")
-            upload(file_destination1+filename)
+        shutil.move(filepath, file_destination1)
+
+for root, dirs, files in os.walk(file_destination1):
+    if (os.path.getsize(os.path.join(root, filename)) >> 20) > 32:  
+        print("\n"+file_destination1+filename + " moved")
+        uploadbig(file_destination1+filename)
+    else:
+        print("\n"+file_destination1+filename + " moved")
+        upload(file_destination1+filename)
 
 shutil.rmtree(file_source)
 os.mkdir(file_source)
